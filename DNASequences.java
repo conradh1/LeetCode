8bd56569a780991de7ca3481aee6a994f68ecea9
conradh1 <conradh1@gmail.com>
2015-10-29 11:36:51 -1000

    Added solution to DNA sequence problem.

---
+++ DNASequences.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class DNASequences
+{
+   public static List<String> findRepeatedDnaSequences(String dna) {
+        Set<String> seqSet = new HashSet<String>();
+        List<String> foundList = new ArrayList<String>();
+        // Need string greater than 10 in length for sequence.
+        
+        int seqSize = 10;
+        if ( dna.length() < seqSize )
+          return foundList;
+        
+                
+        for ( int i = 0; i + 9 < dna.length(); i++) {
+          String seq =  dna.substring(i,seqSize+i);          
+          
+          //System.out.println(seq);
+          //System.out.println(seq+" "+rest);
+          //System.out.println(dna);
+          
+          if ( seqSet.contains(seq) ) {                                    
+            if ( !foundList.contains(seq))
+              foundList.add(seq);
+          }
+          else {
+            seqSet.add(seq);
+          }
+        } 
+        return foundList;
+    }
+  
+  
+  public final static void main(String S[]) {
+    String dna =  "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+    
+    List<String> seqList = new ArrayList<String>();
+    seqList = findRepeatedDnaSequences(dna);
+    Iterator iter = seqList.iterator();
+    while (iter.hasNext()) {      // any more element
+        // Retrieve the next element, explicitly downcast from Object back to String
+        String str = (String)iter.next();
+        System.out.println(str);        
+    }
+            
+  }
+}
